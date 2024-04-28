5ee5cb373beafe0f504332c3e4342f281191e91a
Nguyen-Thanh-Nhann <bizner94@gmail.com>
2024-04-28 11:53:10 +0700

    xong video 18

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,13 +1,38 @@
-const createUser = () => {
+const User = require('../models/UserModel')
+
+const createUser = (newUser) => {
     return new Promise(async(resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = newUser
         try {
-            resolve({})
+            const CheckUser = await User.findOne({ 
+                email: email
+            })
+            if(CheckUser !== null) {
+                resolve({
+                    status: 'ok',
+                    message:' the email is already'
+                })
+            }
+            const createUser = await User.create({
+                name, 
+                email, 
+                password, 
+                confirmPassword,
+                phone
+            })
+            if (createUser){
+                resolve({
+                    status: 'oke',
+                    message:'Success',
+                    data: createUser
+                })
+            }
         }catch(e) {
             reject(e)
         } 
     })
 }
-        
+
 module.exports = {
     createUser
 }
