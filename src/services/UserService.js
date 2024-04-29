ff701a3a183c8cbde2667c7c8e757906671c68e7
Nguyen-Thanh-Nhann <bizner94@gmail.com>
2024-04-29 10:51:47 +0700

    xong video 21

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -101,8 +101,78 @@ const updateUser = (id, data) => {
     })
 }
 
+const deleteUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({
+                _id: id
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The user is not defined'
+                })
+            }
+
+            await User.findByIdAndDelete(id)
+            resolve({
+                status: 'OK',
+                message: 'DELETE USER SUCCESS',
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+const getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            const allUser = await User.find()
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: allUser
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+const getDetailsUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findOne({
+                _id: id
+            })
+            if (user === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The user is not defined'
+                })
+            }
+
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: user
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+getDetailsUser
+
 module.exports = {
     createUser,
     loginUser,
-    updateUser
+    updateUser,
+    deleteUser,
+    getAllUser,
+    getDetailsUser
 }
