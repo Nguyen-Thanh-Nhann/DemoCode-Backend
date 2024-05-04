8e9b64ec65958b99e7561c2276ece6496406de1e
Nguyen-Thanh-Nhann <bizner94@gmail.com>
2024-05-04 17:03:58 +0700

    fix code

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,20 +1,21 @@
 const ProductService = require('../services/ProductService');
 
 
+
 const createProduct = async (req, res) => {
     try {
-        const { name, image, type, countInStock, price, rating, description } = req.body
-
-        if(!name || !image|| !type || !countInStock || !price  || !rating){
-                return res.status(200).json({
-                    status: 'ERR',
-                    message: 'the input is required'
+        const { name, image, type, countInStock, price, rating, description, discount } = req.body
+        if (!name || !image || !type || !countInStock || !price || !rating || !discount) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
             })
         }
-        console.log('response', req.body)
+
         const response = await ProductService.createProduct(req.body)
-        
+
         return res.status(200).json(response)
+
     } catch (e) {
         return res.status(404).json({
             message: e
@@ -27,10 +28,10 @@ const updateProduct = async (req, res) => {
     try {
         const productId = req.params.id
         const data = req.body
-        if(!productId) {
+        if (!productId) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the productId is required'
+                message: 'The productId is required'
             })
         }
         const response = await ProductService.updateProduct(productId, data)
@@ -45,10 +46,10 @@ const updateProduct = async (req, res) => {
 const getDetailsProduct = async (req, res) => {
     try {
         const productId = req.params.id
-        if(!productId) {
+        if (!productId) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the productId is required'
+                message: 'The productId is required'
             })
         }
         const response = await ProductService.getDetailsProduct(productId)
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,20 +6,20 @@ const createUser = async (req, res) => {
         const { name, email, password, confirmPassword, phone } = req.body
         const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
         const isCheckEmail = reg.test(email)
-        if(!name || !email|| !password || !confirmPassword || !phone){
-                return res.status(200).json({
-                    status: 'ERR',
-                    message: 'the input is required'
-            })
-        }else if(!isCheckEmail){
+        if (!email || !password || !confirmPassword) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the input is email'
+                message: 'The input is required'
             })
-        }else if(password !== confirmPassword){
+        } else if (!isCheckEmail) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the password is equal confirmPassword '
+                message: 'The input is email'
+            })
+        } else if (password !== confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal confirmPassword'
             })
         }
         const response = await UserService.createUser(req.body)
@@ -33,29 +33,29 @@ const createUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
     try {
-        console.log(req.body)
-        const { name, email, password, confirmPassword, phone } = req.body
+        const { email, password } = req.body
         const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
         const isCheckEmail = reg.test(email)
-        if(!name || !email|| !password || !confirmPassword || !phone){
-                return res.status(200).json({
-                    status: 'ERR',
-                    message: 'the input is required'
-            })
-        }else if(!isCheckEmail){
+        if (!email || !password) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the input is email'
+                message: 'The input is required'
             })
-        }else if(password !== confirmPassword){
+        } else if (!isCheckEmail) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the password is equal confirmPassword '
+                message: 'The input is email'
             })
         }
-        console.log('isCheckEmail', isCheckEmail)
         const response = await UserService.loginUser(req.body)
-        return res.status(200).json(response)
+        const { refresh_token, ...newReponse } = response
+        res.cookie('refresh_token', refresh_token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+            path: '/',
+        })
+        return res.status(200).json({...newReponse, refresh_token})
     } catch (e) {
         return res.status(404).json({
             message: e
@@ -67,10 +67,10 @@ const updateUser = async (req, res) => {
     try {
         const userId = req.params.id
         const data = req.body
-        if(!userId) {
+        if (!userId) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the userId is required'
+                message: 'The userId is required'
             })
         }
         const response = await UserService.updateUser(userId, data)
@@ -85,10 +85,10 @@ const updateUser = async (req, res) => {
 const deleteUser = async (req, res) => {
     try {
         const userId = req.params.id
-        if(!userId) {
+        if (!userId) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the userId is required'
+                message: 'The userId is required'
             })
         }
         const response = await UserService.deleteUser(userId)
@@ -114,10 +114,10 @@ const getAllUser = async (req, res) => {
 const getDetailsUser = async (req, res) => {
     try {
         const userId = req.params.id
-        if(!userId) {
+        if (!userId) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the userId is required'
+                message: 'The userId is required'
             })
         }
         const response = await UserService.getDetailsUser(userId)
@@ -131,11 +131,11 @@ const getDetailsUser = async (req, res) => {
 
 const refreshToken = async (req, res) => {
     try {
-        const token = req.headers.token.split(' ')[1]
-        if(!token) {
+        let token = req.headers.token.split(' ')[1]
+        if (!token) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'the token is required'
+                message: 'The token is required'
             })
         }
         const response = await JwtService.refreshTokenJwtService(token)
@@ -146,7 +146,6 @@ const refreshToken = async (req, res) => {
         })
     }
 }
-
 
 module.exports = {
     createUser,
